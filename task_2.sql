2074970955be444c7c3a9655f76d65cbcb7d0860
George <george.milad0001@gmail.com>
2025-08-31 04:06:17 +0300

    SQL

---
+++ task_2.sql
@@ -0,0 +1,57 @@
+"""
+Write a script that creates all the tables below in alx_book_store in your MySQL server.
+
+Tables
+
+For each table/relation below, you can find the attributes in task 0
+books
+authors
+customers
+orders
+order details
+Name of the file should be task_2.sql
+
+All SQL keywords should be in uppercase
+
+Repo:
+
+GitHub repository: Intro_to_DB
+File: task_2.sql
+"""
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id) 
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    address TEXT NOT NULL
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
