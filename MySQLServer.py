dfbde661e438bfd63641014b52eea5ece50fbfe0
George <george.milad0001@gmail.com>
2025-08-31 04:02:19 +0300

    SQL

--- MySQLServer.py
+++ MySQLServer.py
@@ -19,7 +19,6 @@ File: MySQLServer.py
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Connect to MySQL server (without specifying database)
@@ -33,7 +32,7 @@ try:
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 finally:
     if 'cursor' in locals():
