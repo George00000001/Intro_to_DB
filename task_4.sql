3c8f37a11aa80db9dd988219aee4694a9c29de8a
George <george.milad0001@gmail.com>
2025-08-31 04:22:04 +0300

    SQL

--- task_4.sql
+++ task_4.sql
@@ -11,4 +11,7 @@ GitHub repository: Intro_to_DB
 File: task_4.sql
 """
 USE alx_book_store;
-SELECT * FROM Books;
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
