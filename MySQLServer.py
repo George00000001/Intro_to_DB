547b7f2e677a58c6b53cb6a482eb9980b7c73fe2
George <george.milad0001@gmail.com>
2025-08-31 04:12:14 +0300

    SQL

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,7 +3,7 @@ Write a simple python script that creates the database alx_book_store in your My
 
 Name of python script should be MySQLServer.py
 If the database alx_book_store already exists, your script should not fail
-You are not allowed to use the SELECT or SHOW statements
+You are not allowed to use the statements
 NOTE :
 
 Required to print message such as Database 'alx_book_store' created successfully! when database is successfully created.
